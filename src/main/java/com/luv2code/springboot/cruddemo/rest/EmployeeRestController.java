86b5c334c964f18deff3ad9f8b39e48058a85c0e
Ricardo Augusto De la Rosa Rosero <ricardo.delarosa@entelgy.com>
2025-01-19 19:49:44 -0500

    Adding method findById to EmployeeRestController class

---
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,37 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeService employeeService;
+
+    // quick and dirty: inject employee dao (use constructor injection)
+    @Autowired
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
+    }
+
+    // expose "/employees" and return a list of employees
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return this.employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable(name="employeeId")int employeeId) {
+       return this.employeeService.findById(employeeId);
+    }
+
+}
